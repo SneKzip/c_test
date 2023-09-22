b75b26130e84c14fb26fb20a888df7747f15dbb3
Kostya <grigo.ko2020@yandex.ru>
2023-09-22 19:45:39 +0500

    test commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// const headline = document.querySelector(".headline");
+// const button = document.querySelector(".button-add");
+
+
+// let post = 
+// `
+//     <div class="card">
+//         <div class="card-body">
+//             <div class = "card-headline">
+//                 <div class = "question">
+//                     <textarea class = "quest form-control" placeholder="Вопрос"></textarea>
+//                 </div>  
+//                 <div class="question-type">
+//                     <select class="form-select">
+//                         <option selected disabled>Вариант ответа</option>
+//                         <option value="1">Текст</option>
+//                         <option value="2">Один из списка</option>
+//                     </select>
+//                 </div>
+//             </div>
+//             <div class = "card-answer">
+//             </div>
+//         </div>
+//     </div>
+// `;
+
+// button.addEventListener("click", ()=>{
+//     headline.insertAdjacentHTML("beforeend", post);
+
+// });
+$(document).ready(()=>{
+    console.log("heli");
+})
